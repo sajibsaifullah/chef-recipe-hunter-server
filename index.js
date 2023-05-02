9c67aa5bce87a14665be5ab1fad8e68c8f6b4557
sajibsaifullah <dusajibphy@gmail.com>
2023-05-02 13:18:45 +0600

    initialized server

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+const cors = require('cors');
+const port = process.env.PORT || 5000;
+
+const allData = require('./data/chefData.json')
+
+
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("Hello, Chef Recipe Hunter is running...");
+});
+
+app.get("/allData", (req, res) => {
+    res.send(allData)
+})
+
+app.listen(port, () => {
+  console.log(`Chef API is running on port ${port}`);
+});
