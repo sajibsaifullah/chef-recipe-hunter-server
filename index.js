7d8039eeda4454d9b84c98e267abd84a199cfdf4
sajibsaifullah <dusajibphy@gmail.com>
2023-05-04 02:44:29 +0600

    data edited

--- index.js
+++ index.js
@@ -16,6 +16,12 @@ app.get("/allData", (req, res) => {
     res.send(allData)
 })
 
+app.get('/allData/:id', (req, res) => {
+  const id = parseInt(req.params.id);
+  const selectedData = allData.find(data => data.id === id);
+  res.send(selectedData);
+})
+
 app.listen(port, () => {
   console.log(`Chef API is running on port ${port}`);
 });
